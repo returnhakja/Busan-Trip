eebda522f3f48bcc307f5861b9f183bdfbb00203
도헌's <khs39200@gmail.com>
2022-06-02 19:33:27 +0900

    add main, index

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Main.module.css";
 import clear from "../img/clear.png";
+import { Link } from "react-router-dom";
+import place from "../api/mock.json";
+
+function getRandomInt(min, max) {
+    console.log("----------");
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    console.log(Math.floor(Math.random() * (max - min)));
+    return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+}
 
 function Main() {
+    // const [random, setRandom] = useState([]);
+
+    let a = Math.floor(Math.random() * 100) + 1;
+    const items = place.getAttractionKr.item;
     return (
         <div className="body-wrapper">
             <div className="maincoutainer">
                 <img className="main-img" src={clear} alt="clear" />
+            </div>
+            <div>
+                {items.map((item, index) => {
+                    if (a === index || a === index + 1 || a === index + 2) {
+                        return (
+                            <span className="image1">
+                                <Link to="Detail">
+                                    <img
+                                        className="image"
+                                        src={item.MAIN_IMG_THUMB}
+                                        alt="gallery"
+                                    />
+                                </Link>
+                                <p>{item.TITLE}</p>
+                            </span>
+                        );
+                    }
+                })}
             </div>
         </div>
     );
